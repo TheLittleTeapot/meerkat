7bf957b05b037f00843fc9b2af0d37bc511189b4
GBWalkerA <Andrew.Walker@sony.com>
2017-07-16 15:37:31 +0100

    Added Ir code enum class
    Added test timers

--- meerkat/bravia_control/src/main.cpp
+++ meerkat/bravia_control/src/main.cpp
@@ -1,20 +1,50 @@
-#include <iostream>
-#include <string>
-
 #include "bravia_controller.h"
 
+#include <iostream>
+#include <string>
+#include <queue>
+#include <chrono>
 
 int main(void)
 {
-	bravia::Controller bravia("192.168.1.116:20060");
+	using irc = bravia::command::IrCode;
 
-	bravia.setIrccCode(30, [](bool res)
+	//bravia::Controller bravia("192.168.1.116:20060");
+	bravia::Controller bravia("192.168.1.4:20060");
+
+	bravia.setIrccCode(irc::Volume_Up, [](bool res)
 	{
 		printf("result of the code was: %s\n", res == 1 ? "true" : "false"); 
 	});
 
+	std::queue<bravia::command::IrCode> codesToExecute;
+
+	codesToExecute.emplace(irc::Netflix);
+	codesToExecute.emplace(irc::Volume_Down);
+	codesToExecute.emplace(irc::Volume_Up);
+	codesToExecute.emplace(irc::Confirm);
+
+	auto then = std::chrono::high_resolution_clock::now();
 	for (;;)
 	{
+		auto now = std::chrono::high_resolution_clock::now();
+
+		if (std::chrono::duration_cast<std::chrono::seconds>(now - then).count() > 10)
+		{
+			if (codesToExecute.empty())
+				break;
+
+			const auto code = codesToExecute.front();
+			codesToExecute.pop();
+
+			bravia.setIrccCode(code, [](bool res)
+			{
+				printf("result of the code was: %s\n", res == 1 ? "true" : "false");
+			});
+
+			then = std::chrono::high_resolution_clock::now();
+		}
+
 		bravia.update();
 	}
 }
